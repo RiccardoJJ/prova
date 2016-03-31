41ee4f14f24302833d7e0bd203352f789974f756
Riccardo <riccardo.merli94@gmail.com>
2016-03-31 19:24:24 +0200

    ciao

--- app/src/main/java/ml/screwdriver/prova_git/MainActivity.java
+++ app/src/main/java/ml/screwdriver/prova_git/MainActivity.java
@@ -2,6 +2,7 @@ package ml.screwdriver.prova_git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"cik",Toast.LENGTH_SHORT).show();
     }
 }
